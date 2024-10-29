8ae54e3909fb7c1b4749a5aaa25befd130b29fd2
LeeHyeonseob <hyeonseob22@gmail.com>
2024-10-29 10:44:26 +0900

    Chore: build.gradle.kts 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,11 +25,43 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-websocket")
+	implementation("org.springframework.boot:spring-boot-starter-data-redis")
 	runtimeOnly("com.mysql:mysql-connector-j")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testImplementation("org.springframework.security:spring-security-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	// Google Cloud Vision API
+//	implementation("com.google.cloud:google-cloud-vision:3.47.0")
+
+	// JSON
+	implementation("org.json:json:20231013")
+
+	// Jackson Databind
+	implementation("com.fasterxml.jackson.core:jackson-databind:2.17.2")
+
+	// Swagger
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
+	// JWT
+	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+	implementation("io.jsonwebtoken:jjwt-impl:0.11.5")
+	implementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
+	implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.17.2")
+
+	// OAuth 2.0
+	implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
+	implementation("io.netty:netty-resolver-dns-native-macos:4.1.92.Final:osx-aarch_64")
+	testImplementation("io.projectreactor:reactor-test")
+
+	/// QueryDSL
+	implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
+	annotationProcessor("com.querydsl:querydsl-apt:5.0.0:jakarta")
+	annotationProcessor("jakarta.annotation:jakarta.annotation-api")
+	annotationProcessor("jakarta.persistence:jakarta.persistence-api")
 }
 
 kotlin {
@@ -44,6 +76,18 @@ allOpen {
 	annotation("jakarta.persistence.Embeddable")
 }
 
+tasks.named("clean") {
+	delete(file("src/main/generated"))
+}
+
+tasks.getByName<Jar>("bootJar") {
+	archiveFileName.set("app.jar")
+}
+
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+springBoot {
+	mainClass.set("com.grepp.nbe1_3_team09.Nbe13Team9Application")
 }
