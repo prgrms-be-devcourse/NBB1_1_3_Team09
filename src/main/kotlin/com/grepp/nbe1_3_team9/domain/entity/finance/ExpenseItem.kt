a78b4471a7b7476702dcd2a4495e5064e86cce88
kahyun0255 <kkh9946@naver.com>
2024-10-31 16:17:59 +0900

    Feature: expenseItem entity 생성

---
+++ src/main/kotlin/com/grepp/nbe1_3_team9/domain/entity/finance/ExpenseItem.kt
@@ -0,0 +1,29 @@
+package com.grepp.nbe1_3_team9.domain.entity.finance
+
+import com.grepp.nbe1_3_team9.domain.entity.group.Group
+import jakarta.persistence.*
+import java.math.BigDecimal
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "expense_item_tb")
+class ExpenseItem (
+    @field:Column(nullable = false, length = 100)
+    var itemName:String,
+
+    @field:Column(nullable = false, precision = 10, scale = 2)
+    var amount: BigDecimal,
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var expenseItemId: Long = 0L,
+){
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "group_id", nullable = false)
+    lateinit var group: Group
+
+    fun updateExpenseItem(itemName: String, amount: BigDecimal) {
+        this.itemName = itemName
+        this.amount = amount
+    }
+}
