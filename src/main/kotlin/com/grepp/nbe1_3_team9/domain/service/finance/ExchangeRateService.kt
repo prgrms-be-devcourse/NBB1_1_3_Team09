f4c31d100b201e6fee02267b347b594a0581e664
kahyun0255 <kkh9946@naver.com>
2024-10-31 10:49:47 +0900

    Refactor: exchangeRate 변환 시간 오류 수정

--- src/main/kotlin/com/grepp/nbe1_3_team9/domain/service/finance/ExchangeRateService.kt
+++ src/main/kotlin/com/grepp/nbe1_3_team9/domain/service/finance/ExchangeRateService.kt
@@ -34,7 +34,7 @@ class ExchangeRateService(
             )
 
             val result = ExchangeRateResDTO(
-                time = exchangeRateTime,
+                time = exchangeRateTime ?: LocalDateTime.now(),
                 toCountry = exchangeRateReqDTO.toCountry,
                 fromCountry = exchangeRateReqDTO.fromCountry,
                 toAmount = BigDecimal(exchangeRateReqDTO.amount),
@@ -148,14 +148,13 @@ class ExchangeRateService(
                 }
             } catch (e: Exception) {
                 //log.warn(">>>> {} : {} <<<<", e, ExchangeRateException(ExceptionMessage.EXCHANGE_ERROR))
-                println(e)
                 throw ExchangeRateException(ExceptionMessage.EXCHANGE_ERROR)
             }
         }
     }
 
     companion object {
-        private var exchangeRateTime: LocalDateTime = LocalDateTime.now()
+        private var exchangeRateTime:LocalDateTime? = null
     }
 
         private val currencyCode = arrayOf("AED", "AFN", "ALL", "AMD", "ANG", "AOA", "ARS", "AUD", "AWG", "AZN", "BAM", "BBD", "BDT", "BGN", "BHD", "BIF", "BMD", "BND", "BOB", "BRL", "BSD", "BTN", "BWP", "BYN", "BZD", "CAD", "CDF", "CHF", "CLP", "CNY", "COP", "CRC", "CUP", "CVE", "CZK", "DJF", "DKK", "DOP", "DZD", "EGP", "ERN", "ETB", "EUR", "FJD", "FKP", "FOK", "GBP", "GEL", "GGP", "GHS", "GIP", "GMD", "GNF", "GTQ", "GYD", "HKD", "HNL", "HRK", "HTG", "HUF", "IDR", "ILS", "IMP", "INR", "IQD", "IRR", "ISK", "JEP", "JMD", "JOD", "JPY", "KES", "KGS", "KHR", "KID", "KMF", "KRW", "KWD", "KYD", "KZT", "LAK", "LBP", "LKR", "LRD", "LSL", "LYD", "MAD", "MDL", "MGA", "MKD", "MMK", "MNT", "MOP", "MRU", "MUR", "MVR", "MWK", "MXN", "MYR", "MZN", "NAD", "NGN", "NIO", "NOK", "NPR", "NZD", "OMR", "PAB", "PEN", "PGK", "PHP", "PKR", "PLN", "PYG", "QAR", "RON", "RSD", "RUB", "RWF", "SAR", "SBD", "SCR", "SDG", "SEK", "SGD", "SHP", "SLE", "SOS", "SRD", "SSP", "STN", "SYP", "SZL", "THB", "TJS", "TMT", "TND", "TOP", "TRY", "TTD", "TVD", "TWD", "TZS", "UAH", "UGX", "USD", "UYU", "UZS", "VES", "VND", "VUV", "WST", "XAF", "XCD", "XDR", "XOF", "XPF", "YER", "ZAR", "ZMW", "ZWL")
