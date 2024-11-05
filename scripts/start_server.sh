987c4ba4b79059a50c83eb9362c7840d863c84f1
ooni <gdbsrjsdn@gmail.com>
2024-11-06 02:49:53 +0900

    Fix: local application.yaml 파일 docker 마운트 부분 scripts에도 추가

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # Docker 컨테이너 실행
-docker run -d -p 8080:8080 --name my-spring-boot-app geonoo/my-spring-boot-app:latest
+docker run -d -p 8080:8080 \
+  -v $(pwd)/src/main/resources/application.yaml:/app/config/application.yaml \
+  --name my-spring-boot-app geonoo/my-spring-boot-app:latest
