c998936c9cba7bba3dcff51e9d6c9912185fdf4c
ooni <gdbsrjsdn@gmail.com>
2024-11-05 11:44:39 +0900

    Feature: 날씨 검색 메서드 & 엔드포인트 추가

--- src/main/kotlin/com/grepp/nbe1_3_team9/controller/weather/WeatherController.kt
+++ src/main/kotlin/com/grepp/nbe1_3_team9/controller/weather/WeatherController.kt
@@ -1,4 +1,17 @@
 package com.grepp.nbe1_3_team9.controller.weather
 
-class WeatherController {
+import com.grepp.nbe1_3_team9.domain.service.weather.WeatherService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class WeatherController(
+    private val weatherService: WeatherService
+) {
+
+    @GetMapping("/forecast")
+    fun getForecast(@RequestParam city: String): String {
+        return weatherService.getForecastByCityName(city)
+    }
 }
--- src/main/kotlin/com/grepp/nbe1_3_team9/domain/service/weather/WeatherService.kt
+++ src/main/kotlin/com/grepp/nbe1_3_team9/domain/service/weather/WeatherService.kt
@@ -1,4 +1,45 @@
 package com.grepp.nbe1_3_team9.domain.service.weather
 
-class WeatherService {
+import com.grepp.nbe1_3_team9.controller.weather.dto.GeocodeRes
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import org.springframework.web.client.RestTemplate
+import org.springframework.web.util.UriComponentsBuilder
+
+@Service
+@Transactional(readOnly = true)
+class WeatherService(
+    private val restTemplate: RestTemplate
+) {
+
+    @Value("\${weather.api.key}")
+    private lateinit var apiKey: String
+
+    // 도시 이름으로 날씨 예보 조회
+    fun getForecastByCityName(city: String): String {
+        // 1. Geocoding API (위도와 경도를 받아옴)
+        val geoCodingUrl = UriComponentsBuilder.fromHttpUrl("http://api.openweathermap.org/geo/1.0/direct")
+            .queryParam("q", city)
+            .queryParam("limit", 1)
+            .queryParam("appid", apiKey)
+            .toUriString()
+
+        val geocodeResponses = restTemplate.getForObject(geoCodingUrl, Array<GeocodeRes>::class.java)
+        if (geocodeResponses.isNullOrEmpty()) {
+            return "City not found"
+        }
+
+        val (lat, lon) = geocodeResponses[0]
+
+        // 2. 5 Day / 3 Hour Forecast API
+        val forecastUrl = UriComponentsBuilder.fromHttpUrl("https://api.openweathermap.org/data/2.5/forecast")
+            .queryParam("lat", lat)
+            .queryParam("lon", lon)
+            .queryParam("appid", apiKey)
+            .queryParam("units", "metric")
+            .toUriString()
+
+        return restTemplate.getForObject(forecastUrl, String::class.java) ?: "Forecast data not found"
+    }
 }
